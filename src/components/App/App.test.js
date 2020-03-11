9fb95f8d3025431a43dde2234797aa59b70fd123
Jacob M <moe.jacobw@gmail.com>
2020-03-11 14:52:49 -0500

    fix icon in tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft, faFrog } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronLeft, faFrog);
 it("renders without crashing", () => {
 	const div = document.createElement("div");
 	ReactDOM.render(
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import { BrowserRouter } from 'react-router-dom'
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft, faFrog } from "@fortawesome/free-solid-svg-icons";
+library.add(faChevronLeft, faFrog);
 describe('Header Component', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div')
